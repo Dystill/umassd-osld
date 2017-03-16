479c2c1924b47f23da061bfce33e12d4f3dfee06
sasora <sfrancis2@umassd.edu>
2017-03-16 04:14:24 -0400

    Merging Metadata Reading with Master Branch

--- OSLD/descriptionfilereader.h
+++ OSLD/descriptionfilereader.h
@@ -3,14 +3,34 @@
 
 #include <QtCore>
 #include <QFileDialog>
-
 #include <QDebug>
+
+
+struct CommonSource {
+    QString sourceNum;
+    QString name;
+    QString type;
+};
+
+struct statusTypes {
+    QString status;
+    QString color;
+    QString textColor;
+    QString typeFaceFlagString;
+    bool    typeFaceFlag;
+};
+
+
 
 class DescriptionFileReader: public QXmlStreamReader
 {
 public:
     DescriptionFileReader(QWidget *parent = 0);
     ~DescriptionFileReader();
+
+    QMap <QString, statusTypes> statuses;
+
+
 
     //Methods
     void readFile(QString filepath);
@@ -21,6 +41,9 @@ public:
     QString getDescription() const;
 
     QString cleanString(QString s);
+    void storeStatusData(QString s, statusTypes t, QString currentTag, QXmlStreamAttributes a);
+    bool StringConvert (statusTypes t);
+    void multiReadNext(int i);
 
     void readMetaData(QString tag);
     void readBlocks();
